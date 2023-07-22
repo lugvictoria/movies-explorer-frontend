2aec0391f36be4d7b02b7036b4448cd311823e67
lugvictoria <victory.senty@yandex.ru>
2023-07-22 18:18:02 +0300

    add NavTab

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,11 +1,12 @@
+import NavTab from '../NavTab/NavTab';
 import Promo from '../Promo/Promo';
 
 function Landing() {
   return (
     <main>
       <Promo />
+      <NavTab />
     </main>
   );
 }
-
 export default Landing;
