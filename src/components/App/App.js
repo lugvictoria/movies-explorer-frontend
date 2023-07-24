99e945aa7c856083e88df1ac72c273ada505a200
lugvictoria <victory.senty@yandex.ru>
2023-07-24 22:23:43 +0300

    route for movies page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import Landing from '../Landing/Landing';
+import Movies from '../Movies/Movies';
 import './App.css';
 
 function App() {
   return (
     <div className="content">
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
