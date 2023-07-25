d33a80a64648f443ca0d200a207dcdf6fab6d32a
lugvictoria <victory.senty@yandex.ru>
2023-07-25 12:36:33 +0300

    add SearchForm and Checkbox

---
+++ src/components/movies/Movies/Movies.js
@@ -0,0 +1,22 @@
+import Footer from '../../common/Header/Header';
+import Header from '../../common/Footer/Footer';
+import Navigation from '../../common/Navigation/Navigation';
+import SearchForm from '../SearchForm/SearchForm';
+
+import './Movies.css';
+
+function Movies() {
+  return (
+    <>
+      <Header>
+        <Navigation />
+      </Header>
+      <main>
+      <SearchForm />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Movies;
