a91e2dc64630ae26e79ada62dbf44bbc23a28a33
lugvictoria <victory.senty@yandex.ru>
2023-07-25 13:10:12 +0300

    add MovieCard

--- src/components/movies/Movies/Movies.js
+++ src/components/movies/Movies/Movies.js
@@ -1,6 +1,7 @@
 import Footer from '../../common/Header/Header';
 import Header from '../../common/Footer/Footer';
 import Navigation from '../../common/Navigation/Navigation';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchForm from '../SearchForm/SearchForm';
 
 import './Movies.css';
@@ -13,6 +14,7 @@ function Movies() {
       </Header>
       <main>
         <SearchForm />
+        <MoviesCardList />
       </main>
       <Footer />
     </>
