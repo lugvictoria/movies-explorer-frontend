53a0ad4e2ac6489af089e5e9e22ffdc755418eb2
lugvictoria <victory.senty@yandex.ru>
2023-07-25 18:09:58 +0300

    add More

--- src/components/movies/Movies/Movies.js
+++ src/components/movies/Movies/Movies.js
@@ -1,11 +1,10 @@
-import Footer from '../../common/Header/Header';
-import Header from '../../common/Footer/Footer';
+import Footer from '../../common/Footer/Footer';
+import Header from '../../common/Header/Header';
 import Navigation from '../../common/Navigation/Navigation';
+import More from '../More/More';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchForm from '../SearchForm/SearchForm';
-
 import './Movies.css';
-
 function Movies() {
   return (
     <>
@@ -15,10 +14,10 @@ function Movies() {
       <main>
         <SearchForm />
         <MoviesCardList />
+        <More />
       </main>
       <Footer />
     </>
   );
 }
-
 export default Movies;
