228230fd0d123d23c2050295a46257d7fe10c1f6
lugvictoria <victory.senty@yandex.ru>
2023-07-26 11:19:36 +0300

    correct

--- src/components/movies/Movies/Movies.js
+++ src/components/movies/Movies/Movies.js
@@ -3,7 +3,6 @@ import Header from '../../common/Header/Header';
 import Navigation from '../../common/Navigation/Navigation';
 import More from '../More/More';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import Preloader from '../Preloader/Preloader';
 import SearchForm from '../SearchForm/SearchForm';
 import './Movies.css';
 function Movies() {
@@ -16,7 +15,7 @@ function Movies() {
         <SearchForm />
         <MoviesCardList type="all" />
         <More />
-        <Preloader />
+       {/* <Preloader /> will ve set later */}
       </main>
       <Footer />
     </>
