7a1fcbe3c0a9038b4a145a7b1e1ef15f9be257fd
lugvictoria <victory.senty@yandex.ru>
2023-07-29 13:23:44 +0300

    images and promo

---
+++ src/components/landing/Promo/Promo.js
@@ -0,0 +1,19 @@
+import './Promo.css';
+
+function Promo() {
+  return (
+    <section className="promo section">
+      <h1 className="promo__title">
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
+      <h2 className="promo__subtitle">
+        Листайте ниже, чтобы узнать больше про этот проект и его создателя.
+      </h2>
+      <h3 className="promo__button">
+       Узнать больше
+      </h3>
+    </section>
+  );
+}
+
+export default Promo;
