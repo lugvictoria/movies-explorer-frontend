076aec6bda0f230d8de6dcaeeef4af90ab578c05
lugvictoria <victory.senty@yandex.ru>
2023-07-31 22:58:36 +0300

    profile-page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Landing from '../landing/Landing/Landing';
 import Page404 from '../Page404/Page404';
+import ProfilePage from '../user/ProfilePage/ProfilePage';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="content">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
---
+++ src/components/user/Profile/Profile.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Profile.css';
+function Profile() {
+
+
+  return (
+    <main className="profile content__stretched-element ">
+      <div className="profile__container">
+      <h1 className="profile__title">Привет, Виталий!</h1>
+        <form className="profile__form">
+          <label className="profile__input-container">
+            <span className="profile__input-label">Имя</span>
+            <input
+              type="text"
+              className="profile__input"
+              name="name"
+              minLength="2"
+              maxLength="30"
+              required={true}
+              placeholder="Виталий"
+            />
+          </label>
+          <label className="profile__input-container">
+            <span className="profile__input-label">E-mail</span>
+            <input
+              type="email"
+              className="profile__input"
+              name="email"
+              required={true}
+              placeholder="pochta@yandex.ru"
+            />
+          </label>
+
+            <>
+              <p className="profile__error-message">
+                При обновлении профиля произошла ошибка.
+              </p>
+
+            </>
+        </form>
+
+
+          <ul className="profile__links">
+            <li className="profile__links-item">
+              <button className="profile__link">
+                Редактировать
+              </button>
+            </li>
+            <li className="profile__links-item">
+              <button className="profile__link profile__link_type_logout">
+                Выйти из аккаунта
+              </button>
+            </li>
+          </ul>
+
+      </div>
+    </main>
+  );
+}
+export default Profile;
