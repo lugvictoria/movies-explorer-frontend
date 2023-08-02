8fd4a56ef6a544cd9b354b727cff5520846db088
lugvictoria <victory.senty@yandex.ru>
2023-08-02 23:38:51 +0300

    design movies 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Landing from '../landing/Landing/Landing';
 import Page404 from '../Page404/Page404';
 import ProfilePage from '../user/ProfilePage/ProfilePage';
+import Movies from '../movies/Movies/Movies';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="content">
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/movies" element={<Movies />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
---
+++ src/components/movies/Movies/Movies.js
@@ -0,0 +1,24 @@
+import Footer from '../../main/Footer/Footer';
+import Header from '../../main/Header/Header';
+import Navigation from '../../main/Navigation/Navigation';
+import More from '../More/More';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import SearchForm from '../SearchForm/SearchForm';
+import './Movies.css';
+function Movies() {
+  return (
+    <>
+      <Header>
+        <Navigation />
+      </Header>
+      <main>
+        <SearchForm />
+        <MoviesCardList type="all" />
+        <More />
+       {/* <Preloader /> will ve set later */}
+      </main>
+      <Footer />
+    </>
+  );
+}
+export default Movies;
