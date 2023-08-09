e3836e1758db9fd90dc1e47eee8c99bc325da22f
lugvictoria <victory.senty@yandex.ru>
2023-08-10 01:05:46 +0300

    flex design

--- src/components/main/AuthLinks/AuthLinks.js
+++ src/components/main/AuthLinks/AuthLinks.js
@@ -5,12 +5,12 @@ function AuthLinks() {
   return (
     <nav className="auth-links">
       <ul className="auth-links__list">
-        <li className="auth-links__list-item">
+        <li className="auth-links__link">
           <NavLink to="/signup" className="auth-links__link">
             Регистрация
           </NavLink>
         </li>
-        <li className="auth-links__list-item">
+        <li className="auth-links__link">
           <NavLink
             to="/signin"
             className="auth-links__link auth-links__link_type_login"
