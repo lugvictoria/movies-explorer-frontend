c21b80b35b052bd8ae44abd678c7c8e31f56fda8
lugvictoria <victory.senty@yandex.ru>
2023-08-10 05:13:01 +0300

    del navigation

--- src/components/movies/SavedMovies/SavedMovies.js
+++ src/components/movies/SavedMovies/SavedMovies.js
@@ -1,6 +1,5 @@
 import Footer from "../../main/Footer/Footer";
 import Header from "../../main/Header/Header";
-import Navigation from "../../main/Navigation/Navigation";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
 import "./SavedMovies.css";
@@ -9,7 +8,6 @@ function SavedMovies() {
   return (
     <div className="movies-page">
       <Header>
-        <Navigation />
       </Header>
       <main>
         <SearchForm />
